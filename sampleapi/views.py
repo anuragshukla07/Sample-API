bffca1b2b902573379b1360c585925f4a117065b
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-10-23 00:32:51 +0530

    '--'

--- sampleapi/views.py
+++ sampleapi/views.py
@@ -2,21 +2,12 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+class Student:
+    name = "Anurag"
+    roll_no = 20
+    marks= 99
+
 @api_view()
 def usersApi(request):
-    users = [
-        {
-            "name" : "Anurag",
-            "role" : "backend"
-        },
-        {
-            "name" : "Pranjal",
-            "role" : "frontend"
-        },
-        {
-            "name" : "Ayush",
-            "role" : "flutter"
-        }
-
-    ]
-    return Response(users)
+   student=Student()
+   return Response(student)
